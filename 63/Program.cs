85be080e2b84418a3f1d80a7e8fdc1cb9505e163
Andrey <5657031@gmail.com>
2023-02-14 16:18:28 +0300

    6

---
+++ 63/Program.cs
@@ -0,0 +1,40 @@
+﻿/*Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+45 -> 101101
+3  -> 11
+2  -> 10*/
+
+int GetNumber (string message)
+{
+    int result = 0;
+    while (true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ошибка ввода, вы ввели не число. Повторите ввод:");
+        }
+    }
+    return result;
+}
+
+
+int [] GetResult (int number)
+{
+    List<int> result = new List<int> ();
+    while (number>0)
+    {
+       result.Add(number%2);
+       number/=2;
+
+    }
+return result.ToArray();
+}
+
+int num = GetNumber ("Введите число: ");
+int [] arr = GetResult(num);
+Array.Reverse(arr);
+Console.Write(String.Join(" ", arr));
